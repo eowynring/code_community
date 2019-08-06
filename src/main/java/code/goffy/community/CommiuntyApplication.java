50763c9bfe3fbf208ffe80cff3244fcfb2c46652
guofei <guofei@163.com>
2019-08-06 09:51:12 +0800

    init repo

---
+++ src/main/java/code/goffy/community/CommiuntyApplication.java
@@ -0,0 +1,13 @@
+package code.goffy.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommiuntyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommiuntyApplication.class, args);
+	}
+
+}
