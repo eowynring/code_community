f4215be774f256f6676b299fa4ebf050a8e77bd6
guofei <guofei@163.com>
2019-08-07 09:08:07 +0800

    add database

--- src/main/java/code/goffy/community/controller/IndexController.java
+++ src/main/java/code/goffy/community/controller/IndexController.java
@@ -1,17 +1,37 @@
 package code.goffy.community.controller;
 
+import code.goffy.community.mapper.UserMapper;
+import code.goffy.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author:Goffy
  */
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String hello(){
+    public String hello(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if("token".equals(cookie.getName())){
+                String tokenValue = cookie.getValue();
+                User user=userMapper.findByToken(tokenValue);
+                if (user != null) {
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
          return "index";
     }
 }
--- src/main/java/code/goffy/community/mapper/UserMapper.java
+++ src/main/java/code/goffy/community/mapper/UserMapper.java
@@ -3,12 +3,16 @@ package code.goffy.community.mapper;
 import code.goffy.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @Author:Goffy
  */
 @Mapper
 public interface UserMapper {
-    @Insert("insert into user(account_id,name,token,gmt_create,gmt_modified)values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    @Insert("insert into user(account_id,name,token,gmt_create,gmt_modified)values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String tokenValue);
 }
